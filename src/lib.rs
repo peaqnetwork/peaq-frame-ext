b689c40cd1b56caf6e7d5a1995a24c270bdb6f4f
Michael Eberhardt <github@michael-eberhardt.de>
2023-04-28 21:40:57 +0300

    added mockup-avg-currency

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,8 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+
 pub mod averaging;
+
+#[cfg(any(feature = "mockup-avg-currency"))]
+pub mod mockups;
---
+++ src/mockups/mod.rs
@@ -0,0 +1,7 @@
+//! TODO
+
+
+#[cfg(any(feature = "mockup-avg-currency"))]
+pub mod avg_currency;
+#[cfg(any(feature = "mockup-avg-currency"))]
+pub use avg_currency::*;
