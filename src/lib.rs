8b82a60e41c1705b154f1eeaa6b53ed614bc4a48
Michael Eberhardt <github@michael-eberhardt.de>
2023-05-02 10:04:06 +0300

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,16 @@
 //! on this crate's trait definitions, you only have to import this crate and not other parts from the
 //! peaq-network-node. The main goal of this crate is independence by using loose coupling of pallets
 //! and very generic reusable implementations.
-//! 
+//!
 //! ### Features
-//! 
-//! Currently, there is one use case for the averaging-capability in the peaq-network-node, that is 
+//!
+//! Currently, there is one use case for the averaging-capability in the peaq-network-node, that is
 //! Currency-based. For this use case we designed a pallet-mockup, to be able to write independent
 //! unit-tests for pallets, that uses the averaging-traits. If you want to use this mockup, the feature
 //! "mockup-avg-currency" needs to be enabled. Feel free to add other datatype-based mockups, or a
 //! datatype-independent mockup.
 
 #![cfg_attr(not(feature = "std"), no_std)]
-
 
 pub mod averaging;
 
--- src/mockups/mod.rs
+++ src/mockups/mod.rs
@@ -1,6 +1,5 @@
 //! TODO
 
-
 #[cfg(any(feature = "mockup-avg-currency"))]
 pub mod avg_currency;
 #[cfg(any(feature = "mockup-avg-currency"))]
